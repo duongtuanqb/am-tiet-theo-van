13338dc4674a09f43b5fc4b9d176db02072659aa
Duong Tuan <duonganhtuantk@gmail.com>
2020-08-20 14:17:26 +0700

    check unicode

--- src/App.vue
+++ src/App.vue
@@ -26,10 +26,16 @@ export default {
   methods: {
     filterSearch() {
       if (this.search) {
+        let search = this.search.toLowerCase();
+        let isUnicode = this.isUnicode(search);
+
         this.textMatched = this.listAmTiet.filter( 
           text => {
             let textLowerCase = text.toLowerCase();
-            let search = this.search.toLowerCase();
+
+            if (isUnicode) {
+              return textLowerCase.includes(search)
+            }
             
             let slug_text = this.slug(textLowerCase);
             return slug_text.includes(search)
@@ -45,14 +51,12 @@ export default {
       str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
       str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
       str = str.replace(/đ/g, "d");
-      str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
-      str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
-      str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
-      str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
-      str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
-      str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
-      str = str.replace(/Đ/g, "D");
       return str;
+    },
+    isUnicode(str) {
+      let regex = RegExp('/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ|è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ|ì|í|ị|ỉ|ĩ|'+
+      'ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ|ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ|ỳ|ý|ỵ|ỷ|ỹ|đ/');
+      return regex.test(str);
     }
   }
 }
